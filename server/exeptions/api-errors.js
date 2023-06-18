c3d1e1b0cc240f99b5e66d07b9f900f6fa21598f
Mikola Kutsko <kn212_kmm@student.ztu.edu.ua>
2023-06-18 23:18:05 +0300

    Add simple error extension

---
+++ server/exeptions/api-errors.js
@@ -0,0 +1,26 @@
+module.exports = class ApiError extends Error {
+    status
+    errors
+
+    constructor(status, message, errors = []) {
+        super(message);
+        this.status = status
+        this.errors = errors
+    }
+
+    static UnauthorizedError(){
+        return new ApiError(401, 'Користувач не авторизований')
+    }
+
+    static BadRequest(message, errors = []){
+        return new ApiError(400, message, errors)
+    }
+
+    static Forbidden(){
+        return new ApiError(403)
+    }
+
+    static MethodNotAllowed(){
+        return new ApiError(405)
+    }
+}
