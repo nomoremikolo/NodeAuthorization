dd93748361ebc4b7ce3dce4843561002409462bf
Mikola Kutsko <kn212_kmm@student.ztu.edu.ua>
2023-06-18 22:42:45 +0300

    Add user model

---
+++ server/models/user-model.js
@@ -0,0 +1,26 @@
+const {Schema, model} = require("mongoose")
+
+const userSchema = new Schema({
+    phone: {
+        type: String,
+        unique: true, 
+        require: true,
+    },
+    email: {
+        type: String, 
+        unique: true, 
+        require: true,
+    },
+    password: {
+        type: String, 
+        require: true,
+    },
+    permissions: {
+        type: String, 
+        value: [String],
+        require: false, 
+        default: [],
+    },
+})
+
+module.exports = model('User', userSchema)
