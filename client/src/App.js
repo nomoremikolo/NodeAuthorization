eccf1910ccb263c94b44bedf5c4a4cc16cb21fea
Mikola Kutsko <kn212_kmm@student.ztu.edu.ua>
2023-06-22 23:05:04 +0300

    Adjust a client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
+import Layout from "components/Layout";
+import Welcome from "pages/Welcome";
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route element={<Layout/>}>
+        <Route path="/" element={<Welcome/>}/>
+      </Route>
+    </Routes>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./assets/bootstrap.min.css"
+import "./assets/styles.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
